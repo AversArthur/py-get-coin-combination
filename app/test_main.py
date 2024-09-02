119da4ec0bd84221c38fa2e5ee14f886c1fa2b5c
Arthur <avers.target@gmail.com>
2024-09-02 13:40:15 +0200

    chore: Add tests for get_coin_combination function

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_have_one_penny():
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_have_6_penny():
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_have_17_penny():
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_have_50_penny():
+    assert get_coin_combination(50) == [0, 0, 0, 2]
